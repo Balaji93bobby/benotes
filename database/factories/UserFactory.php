e0140622d135c33b1f576d6c8a5617d09060f3ee
Balaji jayakumar <balaji93bobby@gmail.com>
2024-03-16 08:58:08 +0530

    Update UserFactory.php

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -19,7 +19,7 @@ class UserFactory extends Factory
             'name' => $this->faker->name(),
             'email' => $this->faker->unique()->safeEmail(),
             //'email_verified_at' => now(),
-            'password' => Hash::make($this->faker->password()),
+            'password' => Hash::make('1234567890'),
             //'remember_token' => Str::random(10),
             'permission' => 7,
         ];
